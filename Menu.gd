83f6e6e52623ddf05e058f1d785bee1637812f73
Mikael Mollberg <MikaelMollberg@users.noreply.github.com>
2022-07-17 16:19:41 +0200

    Add How To Play

--- Menu.gd
+++ Menu.gd
@@ -2,7 +2,18 @@ extends Control
 
 
 func _ready():
-    pass
+    $HowToPlay.hide()
 
 func _on_StartButton_pressed():
     get_tree().change_scene("res://Game.tscn")
+
+func _on_HowToPlayButton_pressed():
+    $Buttons.hide()
+    $HowToPlay/AnimationPlayer.play("coin_move")
+    $HowToPlay.show()
+
+
+func _on_HowToPlayTexture_pressed():
+    $HowToPlay.hide()
+    $HowToPlay/AnimationPlayer.play("RESET")
+    $Buttons.show()
